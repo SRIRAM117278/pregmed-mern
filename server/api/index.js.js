9d5cf09857f997cd25b95a30865ef78e1626ee54
SRIRAM <sriramelangovan5383@gmail.com>
2026-01-22 22:20:13 -0800

    fix

---
+++ server/api/index.js.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend API is running");
+});
+
+// ❌ DO NOT use app.listen()
+
+export default app;
